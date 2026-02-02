cc0757c0184c53111b284fb75fc4f810ec9414a2
arthavjain103 <158511299+arthavjain103@users.noreply.github.com>
2026-02-03 00:36:58 +0530

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,7 @@
 from django.urls import re_path ,path, include
-from django.views.generic import TemplateView
+
 urlpatterns = [
     path('api/', include('core.urls')),
-      re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+      
      
 ]
